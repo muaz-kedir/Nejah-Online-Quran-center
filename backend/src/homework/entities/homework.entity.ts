a7ef940408a5fba42688d496cae3f56d06c7c2d3
neuron <mushas1248@gmail.com>
2026-06-13 13:28:32 +0300

    fix finance service, homework entity and controller

--- backend/src/homework/entities/homework.entity.ts
+++ backend/src/homework/entities/homework.entity.ts
@@ -1,4 +1,12 @@
-import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  JoinColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 import { Student } from '../../students/entities/student.entity';
 
 export enum HomeworkDifficulty {
