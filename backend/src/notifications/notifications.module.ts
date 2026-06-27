545b0948974fdf9386ae6f0c4ba424d13db83d9d
neuron <mushas1248@gmail.com>
2026-06-27 15:50:53 +0300

    feat(backend): register FcmModule components in NotificationsModule

--- backend/src/notifications/notifications.module.ts
+++ backend/src/notifications/notifications.module.ts
@@ -6,6 +6,9 @@ import { Notification } from './entities/notification.entity';
 import { PushSubscription } from './entities/push-subscription.entity';
 import { PushSubscriptionService } from './push-subscription.service';
 import { PushSubscriptionController } from './push-subscription.controller';
+import { FcmToken } from './entities/fcm-token.entity';
+import { FcmService } from './fcm.service';
+import { FcmController } from './fcm.controller';
 import { Student } from '../students/entities/student.entity';
 import { Parent } from '../parents/entities/parent.entity';
 import { User } from '../users/entities/user.entity';
@@ -13,11 +16,11 @@ import { WebsocketModule } from '../websocket/websocket.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Notification, PushSubscription, Student, Parent, User]),
+    TypeOrmModule.forFeature([Notification, PushSubscription, FcmToken, Student, Parent, User]),
     forwardRef(() => WebsocketModule),
   ],
-  controllers: [NotificationsController, PushSubscriptionController],
-  providers: [NotificationsService, PushSubscriptionService],
-  exports: [NotificationsService, PushSubscriptionService, TypeOrmModule],
+  controllers: [NotificationsController, PushSubscriptionController, FcmController],
+  providers: [NotificationsService, PushSubscriptionService, FcmService],
+  exports: [NotificationsService, PushSubscriptionService, FcmService, TypeOrmModule],
 })
 export class NotificationsModule {}
