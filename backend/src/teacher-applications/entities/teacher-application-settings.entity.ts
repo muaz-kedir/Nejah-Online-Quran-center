16e05caf052626e028650d9ed12ca3fe7f409cdf
neuron <mushas1248@gmail.com>
2026-07-10 01:59:23 +0300

    feat(teacher-application): add announcementText JSON column to settings entity

--- backend/src/teacher-applications/entities/teacher-application-settings.entity.ts
+++ backend/src/teacher-applications/entities/teacher-application-settings.entity.ts
@@ -1,5 +1,11 @@
 import { Entity, Column, PrimaryColumn } from 'typeorm';
 
+export type AnnouncementText = {
+  en: string;
+  ar: string;
+  am: string;
+};
+
 @Entity('teacher_application_settings')
 export class TeacherApplicationSettings {
   @PrimaryColumn({ default: 1 })
@@ -7,4 +13,7 @@ export class TeacherApplicationSettings {
 
   @Column({ default: false })
   isApplicationsOpen: boolean;
+
+  @Column({ type: 'json', nullable: true })
+  announcementText: AnnouncementText | null;
 }
